7bf98fe3c8223a38d3ce1ed59d51d4cbff7c3cc5
adryanbabaran <adryanbabaran@gmail.com>
2024-02-08 20:01:01 +0800

    Add s54 codes

--- controllers/order.js
+++ controllers/order.js
@@ -8,33 +8,87 @@ const auth = require("../auth");
 //[SECTION] Checkout
 module.exports.checkout = async (req, res) => {
     try {
-        // Find the cart by cartId and userId
-        const cart = await Cart.findOne({ _id: req.body.cartId, userId: req.user.id });
 
-        if (!cart) {
-            return res.status(404).send({ message: 'Cart not found' });
-        }
+    	if (req.user.isAdmin){
+    		return res.status(403).send({Error : "Admin accounts cannot checkout"})
+    	} else{
+	        // Find the cart by cartId and userId
+	        const cart = await Cart.findOne({ _id: req.body.cartId, userId: req.user.id });
 
-        // Check if the cart is empty
-        if (cart.cartItems.length === 0) {
-            return res.status(409).send({ message: 'Cart is empty' });
-        }
+	        if (!cart) {
+	            return res.status(404).send({ message: 'Cart not found' });
+	        }
 
-        // Create a new order with cartItems and totalPrice
-        const newOrder = new Order({
-            userId: req.user.id,
-            productsOrdered: cart.cartItems,
-            totalPrice: cart.totalPrice
-        });
+	        // Check if the cart is empty
+	        if (cart.cartItems.length === 0) {
+	            return res.status(409).send({ message: 'Cart is empty' });
+	        }
 
-        // Save the new order
-        await newOrder.save();
+	        // Create a new order with cartItems and totalPrice
+	        const newOrder = new Order({
+	            userId: req.user.id,
+	            productsOrdered: cart.cartItems,
+	            totalPrice: cart.totalPrice
+	        });
 
-        return res.status(201).send({ message: 'Order checkout successful',
-        							order: newOrder }
-        );
-    } catch (err) {
+	        // Save the new order
+	        await newOrder.save();
+
+	        return res.status(201).send({ message: 'Order checkout successful',
+	        							order: newOrder }
+	        );
+    	}
+	} catch (err) {
         console.error('Error in checkout: ', err);
         return res.status(500).send({ error: 'Error checking out' });
     }
 };
+
+//[SECTION] Get all orders
+module.exports.getAllOrders = (req, res) => {
+
+    return Order.find({})
+    .then(orders => {
+
+        if(orders.length > 0){
+            return res.status(200).send({ message: "Search complete.",orders: orders });
+        }
+        else{
+            return res.status(200).send({ message: 'No orders found.' });
+        }
+
+    })
+    .catch(err => {
+
+        console.error("Error in finding all orders", err);
+
+        return res.status(500).send({ error: "Error finding orders"});
+
+    });
+
+};
+
+//[SECTION] Get user orders
+module.exports.getMyOrders = (req, res) => {
+
+    return Order.find({userId: req.user.id})
+    .then(orders => {
+
+        if(orders.length > 0){
+            return res.status(200).send({ message: "My Order History", orders });
+        }
+        else{
+            return res.status(200).send({ message: 'No orders found.' });
+        }
+
+    })
+    .catch(err => {
+
+        console.error("Error in finding all orders", err);
+
+        return res.status(500).send({ error: "Error finding orders"});
+
+    });
+
+};
+
--- routes/order.js
+++ routes/order.js
@@ -11,26 +11,13 @@ const router = express.Router();
 // [SECTION] Create Product
 router.post("/checkout", verify, isLoggedIn, orderController.checkout);
 
-/*//[SECTION] Route for retrieving all products
-router.get("/all", verify, verifyAdmin, productController.getAllProducts);
+//[SECTION] Route for retrieving all products
+router.get("/all-orders", verify, verifyAdmin, orderController.getAllOrders);
 
 // [SECTION] Get All Active
-router.get("/active", productController.getAllActive );
+router.get("/my-orders", verify, isLoggedIn, orderController.getMyOrders);
 
-// [SECTION] Retrieve single product
-router.get("/:productId", productController.getProduct );
 
-// [SECTION] Update product information
-router.patch("/:productId", verify, verifyAdmin, productController.updateProduct );
 
-//[SECTION] Route for product archive/activate
-router.patch("/archive/:productId", verify, verifyAdmin, productController.archiveProduct);
-router.patch("/activate/:productId", verify, verifyAdmin, productController.activateProduct);
-
-//[SECTION] Search Product By Name
-router.post("/searchByName", productController.searchByName);
-
-//[SECTION] Search Product By Price Range
-router.post("/searchByPrice", productController.searchByPriceRange);*/
 
 module.exports = router;
